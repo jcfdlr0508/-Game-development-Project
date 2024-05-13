13a3ab90f07a6a31ce2fffcba9bf1782c5ecfeb4
Jireh De La Rosa <jireh732@gmail.com>
2024-05-13 22:33:09 +0800

    archer finished

--- scenes/Main_Character.gd
+++ scenes/Main_Character.gd
@@ -9,27 +9,38 @@ const double_jump_velocity = -450.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jump = false
 var isAttacking = false
+var isBlocking = false
 
 func _physics_process(delta):
 	if sprite_2d.animation == "attack1" and is_on_floor():
 		return
+	elif sprite_2d.animation == "attack1" and not is_on_floor():
+		velocity.y += gravity * delta
+	if sprite_2d.animation == "blocking" and is_on_floor():
+		return
+	elif sprite_2d.animation == "blocking" and not is_on_floor():
+		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("Attacking"):
 		sprite_2d.play("attack1")
 		isAttacking = true
+		isBlocking = false
 		$AttackArea/CollisionShape2D.disabled = false
 
-	if not isAttacking:
+	if Input.is_action_just_pressed("Blocking"):
+		sprite_2d.play("blocking")
+		isBlocking = true
+		isAttacking = false
+
+	if not isAttacking and not isBlocking:
 		if (velocity.x > 1 or velocity.x < -1):
 			sprite_2d.play("running")
-			isAttacking = false
-			
 		else:
 			sprite_2d.play("idle")
 
 		if not is_on_floor():
 			velocity.y += gravity * delta
-			sprite_2d.animation = "jumping"
+			sprite_2d.play("jumping")
 		else:
 			has_double_jump = false
 
@@ -49,7 +60,6 @@ func _physics_process(delta):
 			else:
 				$AttackArea/CollisionShape2D.position.x = 182
 			sprite_2d.animation = "running"
-			isAttacking = false
 		else:
 			if is_on_floor():
 				sprite_2d.animation = "idle"
@@ -57,6 +67,10 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+func _input(event):
+	if event.is_action_released("Blocking"):
+		isBlocking = false
+		sprite_2d.play("idle")
 
 func _on_sprite_2d_animation_finished():
 	if sprite_2d.animation == "attack1":
--- scenes/enemy_archer.gd
+++ scenes/enemy_archer.gd
@@ -10,32 +10,31 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_right = false
 
-
 func _physics_process(delta):
 	#Idling Position
-	if sprite_2d.animation == "Idle":
+	if sprite_2d.animation == "idle":
 		sprite_2d.position.x = -57
+		if sprite_2d.flip_h == false:
+			sprite_2d.position.x = -51
 	#Attack Mechanism
 	if sprite_2d.animation == "attacking":
 		sprite_2d.position.x = -87
-
+		if sprite_2d.flip_h == false:
+			sprite_2d.position.x = -19
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip()
+		if facing_right:
+			velocity.x = speed
+		else:
+			velocity.x = -speed
+	if velocity.x !=0:
+		sprite_2d.play("running")
 	move_and_slide()
-
-
-
-func _on_attack_player_detector_body_entered(body):
-	sprite_2d.play("attacking")
-	velocity.x = 0
-	sprite_2d.position.x = -87
-
-
-func _on_attack_player_detector_body_exited(body):
-	sprite_2d.play("Idle")
-	sprite_2d.position.x = -57
-	
 
 func _on_player_detector_body_entered(body):
 	sprite_2d.play("idle")
@@ -47,8 +46,28 @@ func _on_player_detector_body_entered(body):
 			sprite_2d.play("running")
 			velocity.x = -speed
 
+func _on_attack_player_detector_body_entered(body):
+	sprite_2d.play("attacking")
+	velocity.x = 0
+	sprite_2d.position.x = -87
+	
 
+func _on_attack_player_detector_body_exited(body):
+	sprite_2d.play("idle")
+	sprite_2d.position.x = -57
 
 func _on_player_detector_body_exited(body):
-	sprite_2d.play("Idle")
+	sprite_2d.play("idle")
 	velocity.x = 0
+
+func flip():
+	facing_right = !facing_right
+	
+	if facing_right:
+		sprite_2d.flip_h = false
+		$RayCast2D.position.x = 256
+		$"Attack Player Detector/CollisionShape2D".position.x = 901
+	else:
+		sprite_2d.flip_h = true
+		$RayCast2D.position.x = 223
+		$"Attack Player Detector/CollisionShape2D".position.x = 633
